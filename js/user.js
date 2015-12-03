b7499361976df34a32eb1ca9c58659b52cf04c20
Yutong Pan <yutonp@uw.edu>
2015-12-02 19:51:07 -0800

    user log in/out doesn't work, bugs about changing to log in and sign up and password check

--- js/user.js
+++ js/user.js
@@ -1,6 +1,3 @@
-var ref = new Firebase("https://sscontactapp.firebaseio.com/");
-var user = ref.getAuth();
-
 // only shows login option if user clicks login
 $(document).ready(function(){
     $("#login_id").click(function(){
@@ -10,8 +7,76 @@ $(document).ready(function(){
     });
 });
 
-function checkPass()
-{
+// Create application with dependency 'firebase'
+var myApp = angular.module('myApp', ['firebase']);
+// Bind controller, passing in $scope, $firebaseAuth, $firebaseArray, $firebaseObject
+myApp.controller('myCtrl', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject){
+    // Create a variable 'ref' to reference your firebase storage
+    var ref = new Firebase("https://ss-user-app.firebaseio.com/");
+
+    var usersRef = ref.child("users");
+
+    // Create authorization object that referes to firebase
+    $scope.authObj = $firebaseAuth(ref);
+
+    // Create a firebaseObject of your users, and store this as part of $scope
+    $scope.users = $firebaseObject(usersRef);
+
+    // Test if already logged in
+    var authData = $scope.authObj.$getAuth();
+    if (authData) {
+        $scope.userId = authData.uid;
+    } 
+    // SignUp function
+    $scope.signUp = function() {
+        // Create user
+        $scope.authObj.$createUser({
+            email: $scope.email,
+            password: $scope.password,          
+        })
+
+        // Once the user is created, call the logIn function
+        .then($scope.logIn)
+
+        // Once logged in, set and save the user data
+        .then(function(authData) {
+            $scope.userId = authData.uid;
+            $scope.users[authData.uid] ={
+                username: $scope.username,
+            }
+            $scope.users.$save()
+        })
+
+        // Catch any errors
+        .catch(function(error) {
+            console.error("Error: ", error);
+        });
+    }
+
+    // SignIn function
+    $scope.signIn = function() {
+        $scope.logIn().then(function(authData){
+            $scope.userId = authData.uid;
+        })
+    }
+
+    // LogIn function
+    $scope.logIn = function() {
+        console.log('log in')
+        return $scope.authObj.$authWithPassword({
+            email: $scope.email,
+            password: $scope.password
+        })
+    }
+
+    // LogOut function
+    $scope.logOut = function() {
+        $scope.authObj.$unauth()
+        $scope.userId = false
+    }
+
+
+function checkPass() {
     //Store the password field objects into variables ...
     var pass1 = document.getElementById('pass1');
     var pass2 = document.getElementById('pass2');
